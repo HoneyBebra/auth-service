cbeeb75803388b7e99fa4c933c77344d38034981
seriy_p <seryi_pv@mail.ru>
2026-05-18 22:37:20 +0300

    Added a todo

--- src/repositories/jwt_token.py
+++ src/repositories/jwt_token.py
@@ -5,7 +5,7 @@ from src.repositories.base.jwt_token import BaseJwtTokenRepository
 
 class JwtTokenRepository(BaseJwtTokenRepository):
     def __init__(self, redis_session: Redis) -> None:
-        self.redis_session = redis_session
+        self.redis_session = redis_session  # TODO: Remove "redis"
 
     async def set_token_to_blacklist(self, token: str, expires_in: int) -> None:
         await self.redis_session.setex(

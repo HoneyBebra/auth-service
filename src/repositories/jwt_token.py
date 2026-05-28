9e7add979fc9e36920d1aa0a3fe659ef78a6520a
seriy_p <seryi_pv@mail.ru>
2026-05-28 22:06:12 +0300

    Update jwt_token.py

--- src/repositories/jwt_token.py
+++ src/repositories/jwt_token.py
@@ -5,7 +5,7 @@ from src.repositories.base.jwt_token import BaseJwtTokenRepository
 
 class JwtTokenRepository(BaseJwtTokenRepository):
     def __init__(self, redis_session: Redis) -> None:
-        self.redis_session = redis_session  # TODO: Remove "redis"
+        self.redis_session = redis_session  # TODO: Remove the word "redis"
 
     async def set_token_to_blacklist(self, token: str, expires_in: int) -> None:
         await self.redis_session.setex(

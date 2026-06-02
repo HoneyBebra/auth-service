699baefeafb892f469316c531225782814260e11
seriy_p <seryi_pv@mail.ru>
2026-06-02 19:47:59 +0300

    created/updated at fixes

--- src/models/base.py
+++ src/models/base.py
@@ -1,8 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
+def utc_now() -> datetime:
+    return datetime.now(timezone.utc)
+
+
 class BaseModel(DeclarativeBase):
-    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(default=utc_now)
+    updated_at: Mapped[datetime] = mapped_column(default=utc_now, onupdate=utc_now)
